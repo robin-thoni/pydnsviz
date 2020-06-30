864f13e101e65a58bec33434f943310b2a23c4bf
Robin THONI <robin@rthoni.com>
2020-06-30 18:19:05 +0200

    Fixed typing

--- src/pydnsviz/options.py
+++ src/pydnsviz/options.py
@@ -3,36 +3,36 @@ import typing
 
 class TsigKey:
     def __init__(self):
-        self.name: str = None
-        self.data: str = None
+        self.name: typing.Optional[str] = None
+        self.data: typing.Optional[str] = None
 
 
 class DnsHost:
     def __init__(self):
-        self.host: str = None
+        self.host: typing.Optional[str] = None
         self.port: int = 53
 
 
 class Zone:
     def __init__(self):
-        self.name: str = None
+        self.name: typing.Optional[str] = None
 
 
 class ZoneAxfr(Zone):
     def __init__(self):
+        super().__init__()
         self.host: DnsHost = DnsHost()
         self.key: TsigKey = TsigKey()
-        super(ZoneAxfr, self)
 
 
 class ZoneFile(Zone):
     def __init__(self):
-        self.file: str = None
-        super(ZoneFile, self)
+        super().__init__()
+        self.file: typing.Optional[str] = None
 
 
 class Options:
     def __init__(self):
         self.zones: typing.List[Zone] = []
-        self.last_key: TsigKey = None
+        self.last_key: typing.Optional[TsigKey] = None
         self.last_host: DnsHost = DnsHost()
